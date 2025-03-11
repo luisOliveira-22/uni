d453d5cf7fdaccc599e9ab16fbdd2b3417fd937f
luisOliveira-22 <luisoliveira22@ua.pt>
2025-03-11 11:45:36 +0000

    General Commit

--- 3/c/Bloco 2: Introdução ao ANTLR/bloco2/b2_03/Execute.java
+++ 3/c/Bloco 2: Introdução ao ANTLR/bloco2/b2_03/Execute.java
@@ -17,32 +17,51 @@ public class Execute extends CalculatorBaseVisitor<Long> {
    }
 
    @Override public Long visitExprAddSub(CalculatorParser.ExprAddSubContext ctx) {
-      Long res = null;
-      return visitChildren(ctx);
-      //return res;
+      Long n1 = visit(ctx.expr(0));
+      String op = ctx.op.getText();
+      Long n2 = visit(ctx.expr(1));
+
+      if(op.equals("+"))
+         return n1 + n2;
+      else if(op.equals("-"))
+         return n1 - n2;
+      
+      return null;
    }
 
    @Override public Long visitExprUnary(CalculatorParser.ExprUnaryContext ctx) {
-      Long res = null;
-      return visitChildren(ctx);
-      //return res;
+      Long num = visit(ctx.expr());
+      if(num == null)
+         return null;
+      if(ctx.op.getText().equals("-"))
+         return num * -1;
+      return num;
    }
 
    @Override public Long visitExprInteger(CalculatorParser.ExprIntegerContext ctx) {
-      Long res = null;
-      return visitChildren(ctx);
-      //return res;
+      return Long.parseLong(ctx.Integer().getText());
    }
 
    @Override public Long visitExprParenthesis(CalculatorParser.ExprParenthesisContext ctx) {
-      Long res = null;
-      return visitChildren(ctx);
-      //return res;
+      return visit(ctx.expr());
    }
 
    @Override public Long visitExprMultDivMod(CalculatorParser.ExprMultDivModContext ctx) {
-      Long res = null;
-      return visitChildren(ctx);
-      //return res;
+      Long n1 = visit(ctx.expr(0));
+      String op = ctx.op.getText();
+      Long n2 = visit(ctx.expr(1));
+
+      if(op.equals("*"))
+         return n1 * n2;
+      else if("/"){
+         if(n2 == 0)
+            System.err.print("ERROR: can't divide by 0!\n");
+         else
+            return n1 / n2;
+      }
+      else if(op.equals("%"))
+         return n1 % n2;
+      
+      return null;
    }
 }
