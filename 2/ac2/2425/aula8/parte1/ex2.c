4dc3b8a6099d075e20f3147464a1bdc4c89ac701
luisOliveira-22 <luisoliveira22@ua.pt>
2025-05-09 13:56:08 +0000

    Fix erros

--- 2/ac2/2425/aula8/parte1/ex2.c
+++ 2/ac2/2425/aula8/parte1/ex2.c
@@ -5,13 +5,13 @@ int main(void)
     // Configure Timer T3 (2 Hz with interrupts disabled)
     T3CONbits.TCKPS = 7;    // 1:256 prescaler 
     PR3 = 39062;            // Fout = 20MHz / (32 * (62499 + 1)) = 10 Hz 
-    TMR3 = 0;               // Clear timer T2 count register 
-    T3CONbits.TON = 1;      // Enable timer T2 (must be the last command of the timer configuration sequence) 
+    TMR3 = 0;               // Clear timer T3 count register 
+    T3CONbits.TON = 1;      // Enable timer T3 (must be the last command of the timer configuration sequence) 
 
     // Configure Timer T3 with interrupts enabled 
     IPC3bits.T3IP = 2; // Interrupt priority (must be in range [1..6]) 
-    IEC0bits.T3IE = 1; // Enable timer T2 interrupts 
-    IFS0bits.T3IF = 0; // Reset timer T2 interrupt flag
+    IEC0bits.T3IE = 1; // Enable timer T3 interrupts 
+    IFS0bits.T3IF = 0; // Reset timer T3 interrupt flag
 
     EnableInterrupts();
     while(1)
